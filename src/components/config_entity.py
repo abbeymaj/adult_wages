c4a2725444fe192f8a6b8097b5a34355ac8207d0
abbeymaj <abbey.majumdar@gmail.com>
2024-12-23 17:57:54 -0500

    data transformation

--- src/components/config_entity.py
+++ src/components/config_entity.py
@@ -18,3 +18,12 @@ class DataTransformationConfig():
     This class defines the path to store the preprocessor object.
     '''
     preprocessor_obj_path:str = os.path.join('artifacts', 'preprocessor.pkl')
+
+# Creating a config to to store the transformed datasets
+@dataclass
+class StoreFeatureConfig():
+    '''
+    This class defines the path to store the transformed datasets.
+    '''
+    xform_train_path:str = os.path.join('feature_store', 'xform_train_set.parquet')
+    xform_test_path:str = os.path.join('feature_store', 'xform_test_set.parquet')
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -9,6 +9,9 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import WOE
 from src.utils import convert_to_categorical
+from src.utils import remove_blank_spaces
+from src.utils import recode_target_class
+from src.utils import save_object
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
@@ -42,11 +45,8 @@ class DataTransformation():
         preprocessor : pipeline object - Returns the preprocessor object. 
         =============================================================================
         '''
-        try:
-            # Reading the path for the train dataset
-            #train_data = pd.read_parquet(self.data_ingestion_config.train_data_path)
-            
-            # Defining the numberical features
+        try:          
+            # Defining the numerical features
             num_cols = ['age', 'education-num', 'hours-per-week']
             
             # Defining the sex feature for one hot encoding
@@ -125,10 +125,55 @@ class DataTransformation():
         '''
         try:
             # Reading the train and test datasets
+            train_df = pd.read_parquet(self.data_ingestion_config.train_data_path)
+            test_df = pd.read_parquet(self.data_ingestion_config.test_data_path)
                         
-            # Dropping fnlwgt features if the features 
-            # exist in the dataset
-            pass
+            # Instantiating the preprocessor object
+            preprocessor_obj = self.create_data_transformation_object()
+            
+            # Dropping fnlwgt feature if this feature exists in the train dataset
+            if 'fnlwgt' in list(train_df.columns):
+                train_df.drop(labels=['fnlwgt'], axis=1, inplace=True)
+            
+            # Dropping the fnlwgt feature if this feature exists in the test dataset
+            if 'fnlwgt' in list(test_df.columns):
+                test_df.drop(labels=['fnlwgt'], axis=1, inplace=True)
+            
+            # Removing any spaces in the features and recoding the target class in the 
+            # train dataset
+            train_df_clean = train_df.pipe(remove_blank_spaces).pipe(recode_target_class)
+            
+            # Removing any spaces in the features and recoding the target class in the 
+            # test dataset
+            test_df_clean = test_df.pipe(remove_blank_spaces).pipe(recode_target_class)
+            
+            # Creating train feature and target sets
+            input_feature_train_df = train_df_clean.drop(labels=['target_class'], axis=1)
+            input_target_train_df = train_df_clean[['target_class']]
+            
+            # Creating test feature and target sets
+            input_feature_test_df = test_df_clean.drop(labels=['target_class'], axis=1)
+            input_target_test_df = test_df_clean[['target_class']]
+            
+            # Transforming the train and test datasets
+            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+            
+            # Combining the transformed train and test datasets with their respective
+            # target sets
+            input_train_combined = pd.concat([input_feature_train_arr, input_target_train_df], axis=1)
+            input_test_combined = pd.concat([input_feature_test_arr, input_target_test_df], axis=1)
+            
+            # Saving the preprocessor object
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_path,
+                object=preprocessor_obj
+            )
+            
+            return(
+                input_train_combined,
+                input_test_combined
+            )
         
         except Exception as e:
             raise CustomException(e, sys)
