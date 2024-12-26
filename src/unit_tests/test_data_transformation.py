c6c1a263e8de6237ef21c8590bf29d58c8b8cc44
abbeymaj <abbey.majumdar@gmail.com>
2024-12-26 18:43:13 -0500

    data transformation unit tests

--- src/unit_tests/test_data_transformation.py
+++ src/unit_tests/test_data_transformation.py
@@ -1,10 +1,13 @@
 # Importing packages
 import sys
+import os
 import pytest
 import pandas as pd
 from sklearn import set_config
 set_config(transform_output='pandas')
 from src.components.config_entity import DataIngestionConfig
+from src.components.config_entity import DataTransformationConfig
+from src.components.config_entity import StoreFeatureConfig
 from src.utils import remove_blank_spaces
 from src.utils import remove_question_mark
 from src.utils import recode_target_class
@@ -22,6 +25,24 @@ def train_data_path():
 def test_data_path():
     test_data_config = DataIngestionConfig()
     return test_data_config.test_data_path
+
+# Creating a function to define the proprocessor object path
+@pytest.fixture(scope='function')
+def preprocessor_object_path():
+    path_preprocessor_obj = DataTransformationConfig()
+    return path_preprocessor_obj.preprocessor_obj_path
+
+# Creating a function to return the transformed train dataset path
+@pytest.fixture(scope='function')
+def xform_train_dataset_path():
+    train_data_transform = StoreFeatureConfig()
+    return train_data_transform.xform_train_path
+
+# Creating a function to return the transformed test dataset path
+@pytest.fixture(scope='function')
+def xform_test_dataset_path():
+    test_data_transform = StoreFeatureConfig()
+    return test_data_transform.xform_test_path
 
 # Creating a function to check whether the object columns do not contain 
 # blank spaces in the train dataset
@@ -95,3 +116,46 @@ def test_preprocessor_object_train_set(train_data_path):
     preprocessor_obj = data_transformation.create_data_transformation_object()
     X_fin = preprocessor_obj.fit_transform(X, y)
     assert len(list(X_fin.columns)) == 16
+
+# Creating a function to test if the preprocessor object is located in the
+# artifacts folder
+def test_preprocessor_object_path(preprocessor_object_path):
+    assert os.path.exists(preprocessor_object_path) is True
+
+# Creating a function to test that the feature store folder exists
+def test_feature_store_folder_exists(path='feature_store'):
+    assert os.path.exists(path) is True
+
+# Creating a function to verify that the transformed train dataset is 
+# present in the feature store folder
+def test_xform_train_dataset_exists(xform_train_dataset_path):
+    assert os.path.exists(xform_train_dataset_path) is True
+
+# Creating a function to verify that the transformed test dataset is
+# present in the feature store folder
+def test_xform_test_dataset_exists(xform_test_dataset_path):
+    assert os.path.exists(xform_test_dataset_path) is True
+
+# Creating a function to test the length of the transformed train dataset
+def test_xform_train_dataset_col_length(xform_train_dataset_path):
+    xform_train_df = pd.read_parquet(xform_train_dataset_path)
+    assert len(list(xform_train_df.columns)) == 17
+
+# Creating a function to test the length of the transformed test dataset
+def test_xform_test_dataset_col_length(xform_test_dataset_path):
+    xform_test_df = pd.read_parquet(xform_test_dataset_path)
+    assert len(list(xform_test_df.columns)) == 17
+
+# Creating a function to test that the "target_class" feature exists in the 
+# transformed train dataset
+def test_targetclass_exists_in_xform_train_dataset(xform_train_dataset_path):
+    xform_train_df = pd.read_parquet(xform_train_dataset_path)
+    cols_list = list(xform_train_df.columns)
+    assert 'target_class' in cols_list
+
+# Creating a function to test that the "target_class" feature exists in the
+# transformed test dataset
+def test_targetclass_exists_in_xform_test_dataset(xform_test_dataset_path):
+    xform_test_df = pd.read_parquet(xform_test_dataset_path)
+    cols_list = list(xform_test_df.columns)
+    assert 'target_class' in cols_list
