3f8129700041c24d75a2d7f106a5d2b0efdb72ac
abbeymaj <abbey.majumdar@gmail.com>
2025-01-25 20:27:43 -0500

    updated training script

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,6 +5,7 @@ import mlflow
 from src.components.model_trainer import ModelTrainer
 
 if __name__ == '__main__':
+    mlflow.set_experiment('training_pipeline')
     subprocess.call('./start_mlflow_server.sh', shell=True)
     with mlflow.start_run(run_name='training_pipeline') as run:
         run_id = run.info.run_id
