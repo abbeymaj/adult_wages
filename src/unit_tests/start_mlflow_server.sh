c82b38341dd4accd4a14f623b15054541d5f3cac
abbeymaj <abbey.majumdar@gmail.com>
2025-02-01 22:34:37 -0500

    unit test changes

--- src/unit_tests/start_mlflow_server.sh
+++ src/unit_tests/start_mlflow_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 nohup mlflow server \
-	--backend-store-uri sqlite:///mlflow.db \
-	--default-artifact-root ./artifacts \
-	--host 0.0.0.0 &
+	--backend-store-uri sqlite:///model_db/mlflow.db \
+	--default-artifact-root ./model_db \
+	--host 0.0.0.0 \
+	--port 5001 &
